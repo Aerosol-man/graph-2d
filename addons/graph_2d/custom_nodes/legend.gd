1681e8a01875d6f9fb7aa83781d08ded2998f86e
Aerosol-man <65870273+Aerosol-man@users.noreply.github.com>
2023-03-14 11:30:10 +0000

    add legend background

--- addons/graph_2d/custom_nodes/legend.gd
+++ addons/graph_2d/custom_nodes/legend.gd
@@ -1,5 +1,5 @@
 tool
-extends Control
+extends Panel
 
 var default_font: Font
 var legend_array: Array # [label: String, color: Color, pos: Vector2]
@@ -8,5 +8,13 @@ func _ready():
 	default_font = Control.new().get_font("font")
 
 func _draw():
+	var new_rect := Rect2()
+
 	for legend in legend_array:
 		draw_string(default_font, legend[2], legend[0], legend[1])
+	
+	_update_box(new_rect)
+
+func _update_box(rect_extents):
+	rect_position = rect_extents.position
+	rect_size = rect_extents.size
